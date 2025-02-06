d07cc76cc4656aacbb6e2f8edce97c02adafe25c
Vishnuthan09 <tvishnuthan@gmail.com>
2025-02-06 11:40:12 +0100

    on a fait une page d'intro ,une page ou on peut choisir les niveaux et le sys de vie

---
+++ niveaux/1/script.js
@@ -0,0 +1,47 @@
+//selectionner les éléments html
+let container = document.querySelector('.container');
+let btn = document.querySelector('.start_btn');
+let scoreContainer = document.querySelector('.score');
+let timeContainer = document.querySelector ('.time');
+
+btn.onclick = function(){
+    let score = 0;
+    let time = 20;
+    let vie = 3;
+    container.innerHTML = "";
+
+    let interval = setInterval( function showtarget(){
+        //creation de la cible 
+        let target = document.createElement('img');
+        target.id= "target";
+        target.src="../../img/silly.png";
+        container.appendChild(target) ;
+        target.style.top = Math.random() * (500 - target.offsetHeight) + 'px';
+        target.style.left = Math.random() * (600 - target.offsetWidth) + 'px';
+
+        //faire disparaitre la cible
+        setTimeout(function(){
+            target.remove();
+            vie=vie-1;
+        },2000)
+
+        //quand on clique sur le target
+        target.onclick =function(){
+            score += 1 ; 
+            target.style.display = 'none' ;
+        }
+        time-=1; // time = time - 1
+
+        //afficher les infos
+        scoreContainer.innerHTML = `Score : ${score}`
+        timeContainer.innerHTML = `Temps :${time}`
+
+        //fin du jeu quand le temps est ecoulé
+        if(time==0){
+
+            clearInterval(interval);
+            container.innerHTML= "le jeu est terminé zebiiii casse toi"    
+        }
+    }
+    ,1000);
+}
--- niveaux/2/script2.js
+++ niveaux/2/script2.js
@@ -3,17 +3,19 @@ let container = document.querySelector('.container');
 let btn = document.querySelector('.start_btn');
 let scoreContainer = document.querySelector('.score');
 let timeContainer = document.querySelector ('.time');
+let vieContainer = document.querySelector ('.vie');
 
 btn.onclick = function(){
     let score = 0;
     let time = 20;
+    let vie = 3;
     container.innerHTML = "";
 
     let interval = setInterval( function showtarget(){
         //creation de la cible 
         let target = document.createElement('img');
         target.id= "target";
-        target.src="silly.png";
+        target.src="../../img/silly.png";
         container.appendChild(target) ;
         target.style.top = Math.random() * (500 - target.offsetHeight) + 'px';
         target.style.left = Math.random() * (600 - target.offsetWidth) + 'px';
@@ -21,6 +23,7 @@ btn.onclick = function(){
         //faire disparaitre la cible
         setTimeout(function(){
             target.remove();
+            vie=vie-1;
         },2000)
 
         //quand on clique sur le target
@@ -33,11 +36,13 @@ btn.onclick = function(){
         //afficher les infos
         scoreContainer.innerHTML = `Score : ${score}`
         timeContainer.innerHTML = `Temps :${time}`
+        vieContainer.innerHTML = `vie : ${vie}`
 
         //fin du jeu quand le temps est ecoulé
         if(time==0){
+
             clearInterval(interval);
-            container.innerHTML= "le jeu est terminé zebiii av"    
+            container.innerHTML= "le jeu est terminé zebiiii casse toi"    
         }
     }
     ,1000);
